605ff5b71c3fcd500445fb1a0ae2896a59db70c4
MerelMoehn <merelmoehn@outlook.com>
2022-09-16 09:21:12 +0000

    Feat: add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type")=== "submit"){
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
-
         })
     }
 })
 
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
